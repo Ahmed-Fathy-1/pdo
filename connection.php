557cfca5f7cb005993c7762288c5be4dfbf6f4ad
Ahmed Fathy <ahmed.fathy@mitchdesigns.com>
2025-03-22 05:25:08 +0200

    create connection pdo file

---
+++ connection.php
@@ -0,0 +1,13 @@
+<?php 
+
+try{
+
+    $pdo = new PDO('mysql:host:localhost;dbname=pdo', 'root', '', [
+        PDO::ATTR_PERSISTENT => true,
+        PDO::ERRMODE_EXCEPTION => true
+    ]); 
+
+} catch (PDOException $e) {
+    die( $e->getMessage() );
+}
+
